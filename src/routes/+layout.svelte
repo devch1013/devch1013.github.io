72cd265770e86f92e5c9a3145a4f314b4d2f1aef
devch1013 <devch1013@gmail.com>
2023-05-14 23:32:18 +0900

    add navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<nav>
+	<div class="navbar-top">
+		<span class="navbar-title text">ChanHyuk's Log</span>
+		<div class="navbar-btn">
+			<a href="/" class="navbar-btn-text text">Home</a>
+			<a href="/blog" class="navbar-btn-text text">Blog</a>
+		</div>
+	</div>
+</nav>
+
+<slot />
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+
+	:global(.text) {
+		font-family: 'Pretendard';
+		font-style: normal;
+		color: #ffffff;
+	}
+	.navbar-top {
+		background-color: black;
+		height: 80px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.navbar-title {
+		font-weight: 700;
+		font-size: 40px;
+		margin-left: 30px;
+	}
+
+	.navbar-btn-text {
+		font-weight: 500;
+		font-size: 25px;
+		margin-left: 30px;
+	}
+
+	.navbar-btn {
+		margin-right: 100px;
+		display: flex;
+		width: 300px;
+		justify-content: space-between;
+	}
+</style>
---
+++ src/routes/blog/[slug]/+page.js
@@ -0,0 +1,9 @@
+import { error } from '@sveltejs/kit';
+
+export function load({ params }) {
+        return {
+            title: params.slug,
+            content: 'Welcome to our blog. Lorem ipsum dolor sit amet...'
+        };
+
+}
